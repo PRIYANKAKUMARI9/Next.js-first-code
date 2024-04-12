70ed35406df2aa3ab556232445792540e2f4a09a
pk <pkji@.com>
2024-04-12 05:32:47 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <h1 className=" bg-slate-800 text-red-600 p-6 rounded-xl">hello priyanka </h1>
+    </main>
+  );
+}
